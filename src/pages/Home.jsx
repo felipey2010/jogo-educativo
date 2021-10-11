060875b102a07633bbc2b37f5826f9849ff74fad
pakpanyi <pakpanyi@gmail.com>
2021-10-11 14:15:46 -0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import "../styles/home.css";
+
+export default function Home() {
+  return (
+    <div className="home-main">
+      <div className="home-title">Jogo Educativo</div>
+      <div className="home-main-container">
+        Apresentação do Protótipo do jogo da disciplina
+      </div>
+    </div>
+  );
+}
+
+// Image from https://i.ytimg.com/vi/dg9cs7eOOs4/maxresdefault.jpg
